f99b269f58f74a4b22cea91218d81acffaa2609a
Anna Melnikova <amelniko@student.42.fr>
2025-03-27 22:43:09 +0100

    Copying files from ex00 to ex01

---
+++ ex01/includes/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+# define MAXGRADE 1
+# define MINGRADE 150
+
+class Bureaucrat
+{
+    private:
+        const std::string _name;
+        int _grades;
+
+    public:
+        Bureaucrat(const std::string& name, int grade);
+        Bureaucrat(const Bureaucrat& other);
+        ~Bureaucrat(void);
+        Bureaucrat& operator=(const Bureaucrat& other);
+
+        std::string getName(void) const;
+        int getGrade(void) const;
+        void increaseGrade(void);
+        void decreaseGrade(void);
+
+    class GradeTooHighException : public std::exception
+    {
+        public:
+            virtual const char* what(void) const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+        public:
+            virtual const char* what(void) const throw();
+    };
+};
+
+std::ostream &operator<<(std::ostream& out, const Bureaucrat& person);
---
+++ ex01/srcs/Bureaucrat.cpp
@@ -0,0 +1,91 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+    : _name(name),
+    _grades(grade)
+{
+    if (_grades < MAXGRADE)
+        throw Bureaucrat::GradeTooHighException();
+    if (_grades > MINGRADE)
+        throw Bureaucrat::GradeTooLowException();
+    std::cout << "Bureaucrat with " << _name << " name and " << _grades << " grade is created" << std::endl;
+    return ;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+    : _name(other._name),
+    _grades(other._grades)
+{
+    if (_grades < MAXGRADE)
+        throw Bureaucrat::GradeTooHighException();
+    if (_grades > MINGRADE)
+        throw Bureaucrat::GradeTooLowException();
+    std::cout << "Bureaucrat with " << _name << " name and " << _grades << " grade is created" << std::endl;
+    return ;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+    return ;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+    if (this != &other)
+        this->_grades = other._grades;
+    if (_grades < MAXGRADE)
+        throw Bureaucrat::GradeTooHighException();
+    if (_grades > MINGRADE)
+        throw Bureaucrat::GradeTooLowException();
+    std::cout << "Bureaucrat with " << _name << " name and " << _grades << " grade is copied" << std::endl;
+    return (*this);
+}
+
+std::string Bureaucrat::getName(void) const
+{
+    std::string ret;
+    ret = this->_name;
+    return (ret);
+}
+
+int Bureaucrat::getGrade(void) const
+{
+    return (this->_grades);
+}
+
+void Bureaucrat::increaseGrade(void)
+{
+    if (_grades - 1< MAXGRADE)
+        throw Bureaucrat::GradeTooHighException();
+    --(this->_grades);
+    return ;
+}
+
+void Bureaucrat::decreaseGrade(void)
+{
+    if (_grades + 1> MINGRADE)
+        throw Bureaucrat::GradeTooLowException();
+    ++(this->_grades);
+    return ;
+}
+
+const char* Bureaucrat::GradeTooHighException::what(void) const throw()
+{
+    std::cout << "Exception caught" << std::endl;
+    return ("The grade of bureaucrat can't be higher than 1");
+}
+
+const char* Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+    std::cout << "Exception caught" << std::endl;
+    return ("The grade of bureaucrat can't be lower than 150");
+}
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& person)
+{
+    out << person.getName();
+    out << ", bureaucrat grade ";
+    out << person.getGrade();
+    out << "." << std::endl;
+    return (out);
+}
