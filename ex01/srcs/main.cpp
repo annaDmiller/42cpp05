2d1a94489dc93b47da5b1a0bdd9f8fc6c0d66e01
Anna Melnikova <amelniko@student.42.fr>
2025-03-29 19:25:37 +0100

    Ex01 is done

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -3,5 +3,52 @@
 
 int main(void)
 {
+    {
+        try
+        {
+            Form test("Test", false, 0, 150);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }        
+    }
+    std::cout << std::endl;
+    {
+        try
+        {
+            Form test("Test", false, 1, 151);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }        
+    }
+    std::cout << std::endl;
+    {
+        Form test("Test", false, 4, 38);
+        Bureaucrat person("Adam", 5);
+        try
+        {
+            person.signForm(test);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }        
+    }
+    std::cout << std::endl;
+    {
+        Form test("Test", false, 5, 38);
+        Bureaucrat person("Adam", 5);
+        try
+        {
+            person.signForm(test);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }        
+    }
     return (0);
 }
